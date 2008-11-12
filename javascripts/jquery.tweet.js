4d36d9df6380486c072317135bb0e680f4615aa1
todd <todd@seaofclouds.com>
2008-11-11 16:34:53 -0800

    fix relative date

--- javascripts/jquery.tweet.js
+++ javascripts/jquery.tweet.js
@@ -9,28 +9,25 @@
     };
     
     function relative_time(time_value) {
-      var values = time_value.split(" ");
-      time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
       var parsed_date = Date.parse(time_value);
       var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
       var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-      delta = delta + (relative_to.getTimezoneOffset() * 60);
-      if (delta < 60) {
-        return 'less than a minute ago';
+      if(delta < 60) {
+      return 'less than a minute ago';
       } else if(delta < 120) {
-        return 'about a minute ago';
+      return 'about a minute ago';
       } else if(delta < (45*60)) {
-        return (parseInt(delta / 60)).toString() + ' minutes ago';
+      return (parseInt(delta / 60)).toString() + ' minutes ago';
       } else if(delta < (90*60)) {
-        return 'about an hour ago';
+      return 'about an hour ago';
       } else if(delta < (24*60*60)) {
-        return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+      return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
       } else if(delta < (48*60*60)) {
-        return '1 day ago';
+      return '1 day ago';
       } else {
-        return (parseInt(delta / 86400)).toString() + ' days ago';
+      return (parseInt(delta / 86400)).toString() + ' days ago';
       }
-    };
+    }
     
     if(o) $.extend(s, o);
     return this.each(function(){

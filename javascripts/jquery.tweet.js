796ccdb958dd2e256afed31813f730431c9135dd
todd <todd@seaofclouds.com>
2008-11-20 10:28:08 -0800

    style fixes for demo

--- javascripts/jquery.tweet.js
+++ javascripts/jquery.tweet.js
@@ -2,7 +2,7 @@
   $.fn.tweet = function(o){
     var s = {
       username: ["seaofclouds"],              // [string]   required, unless you want to display our tweets. :) it can be an array, just do ["username1","username2","etc"]
-      avatar_size: null,                      // [integer]  height and width of avatar if displayed (24 and 32 look good)
+      avatar_size: null,                      // [integer]  height and width of avatar if displayed (48px max)
       count: 1,                               // [integer]  how many tweets to display?
       intro_text: null,                       // [string]   do you want text BEFORE your your tweets?
       outro_text: null,                       // [string]   do you want text AFTER your tweets?

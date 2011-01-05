80ff0d3f330538ca0d037c77a9ecc3be8cb15f6d
Steve Purcell <steve@sanityinc.com>
2011-01-05 10:33:42 +0000

    Include comment about origin of new url regex

--- tweet/jquery.tweet.js
+++ tweet/jquery.tweet.js
@@ -27,6 +27,7 @@
     $.fn.extend({
       linkUrl: function() {
         var returning = [];
+        // See http://daringfireball.net/2010/07/improved_regex_for_matching_urls
         var regexp = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
         this.each(function() {
           returning.push(this.replace(regexp,"<a href=\"$1\">$1</a>"));

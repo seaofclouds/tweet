345d342694d17f1d338e391d3c91634108b93cb7
Steve Purcell <steve@sanityinc.com>
2012-05-17 20:37:26 +0100

    Improved url regex, aimed at web URLs only.
    This should better handle problematic cases like "through:http://t.co/EBi59t2D".

--- tweet/jquery.tweet.js
+++ tweet/jquery.tweet.js
@@ -42,7 +42,7 @@
     }, o);
 
     // See http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-    var url_regexp = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
+    var url_regexp = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
 
     // Expand values inside simple string templates with {placeholders}
     function t(template, info) {

781b1e08af2fe268bb4ac31ca0ebd006a4ff1b0c
Steve Purcell <steve@sanityinc.com>
2012-07-09 14:54:13 +0100

    Use original user's avatar when displaying retweets (fixes #151, see #259)

--- tweet/jquery.tweet.js
+++ tweet/jquery.tweet.js
@@ -179,9 +179,8 @@
       // The actual user name is not returned by all Twitter APIs, so please do not
       // file an issue if it is empty:
       o.name = item.from_user_name || item.user.name;
-      o.avatar_size = s.avatar_size;
-      o.avatar_url = extract_avatar_url(item, (document.location.protocol === 'https:'));
       o.retweet = typeof(item.retweeted_status) != 'undefined';
+
       o.tweet_time = parse_date(item.created_at);
       o.join_text = s.join_text == "auto" ? build_auto_join_text(item.text) : s.join_text;
       o.tweet_id = item.id_str;
@@ -198,11 +197,16 @@
       o.tweet_text = $([linkURLs(o.tweet_raw_text, o.entities)]).linkUser().linkHash()[0];
       o.tweet_text_fancy = $([o.tweet_text]).makeHeart()[0];
 
+      o.avatar_size = s.avatar_size;
+      o.avatar_url = extract_avatar_url(o.retweet ? item.retweeted_status : item, (document.location.protocol === 'https:'));
+      o.avatar_screen_name = o.retweet ? item.retweeted_status.user.screen_name : o.name;
+      o.avatar_profile_url = o.twitter_base+o.avatar_screen_name;
+
       // Default spans, and pre-formatted blocks for common layouts
       o.user = t('<a class="tweet_user" href="{user_url}">{screen_name}</a>', o);
       o.join = s.join_text ? t(' <span class="tweet_join">{join_text}</span> ', o) : ' ';
       o.avatar = o.avatar_size ?
-        t('<a class="tweet_avatar" href="{user_url}"><img src="{avatar_url}" height="{avatar_size}" width="{avatar_size}" alt="{screen_name}\'s avatar" title="{screen_name}\'s avatar" border="0"/></a>', o) : '';
+        t('<a class="tweet_avatar" href="{avatar_profile_url}"><img src="{avatar_url}" height="{avatar_size}" width="{avatar_size}" alt="{avatar_screen_name}\'s avatar" title="{avatar_screen_name}\'s avatar" border="0"/></a>', o) : '';
       o.time = t('<span class="tweet_time"><a href="{tweet_url}" title="view tweet on twitter">{tweet_relative_time}</a></span>', o);
       o.text = t('<span class="tweet_text">{tweet_text_fancy}</span>', o);
       o.reply_action = t('<a class="tweet_action tweet_reply" href="{reply_url}">reply</a>', o);
